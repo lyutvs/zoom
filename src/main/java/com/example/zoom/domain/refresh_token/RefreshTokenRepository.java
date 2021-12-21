ee8353bdaddc7e7d4d2a999bc50cba3bfa257ee9
lyutvs <lyutvs@gmail.com>
2021-12-21 14:52:38 +0900

    📑 :: RefreshTokenDao

---
+++ src/main/java/com/example/zoom/domain/refresh_token/RefreshTokenRepository.java
@@ -0,0 +1,6 @@
+package com.example.zoom.domain.refresh_token;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+}
