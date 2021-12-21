1656ea037258c9bed3b4c0f12b8bb4304e1443e7
lyutvs <lyutvs@gmail.com>
2021-12-22 00:06:42 +0900

    ♻️ :: 파일 위치 변경

--- src/main/java/com/example/zoom/domain/refresh_token/RefreshTokenRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.zoom.domain.refresh_token;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-}
