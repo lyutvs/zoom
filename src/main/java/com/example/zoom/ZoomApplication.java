e7f038e4738835eb3ccd98f6e254a023c5d13329
lyutvs <lyutvs@gmail.com>
2021-12-23 15:31:48 +0900

    ⚡️ :: 401 추가

--- src/main/java/com/example/zoom/ZoomApplication.java
+++ src/main/java/com/example/zoom/ZoomApplication.java
@@ -1,8 +1,10 @@
 package com.example.zoom;
 
+import com.example.zoom.global.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @ConfigurationPropertiesScan
