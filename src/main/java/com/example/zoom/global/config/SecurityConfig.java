34bf794bb60e4c671cc5045bb4584ad5f5ce82be
lyutvs <lyutvs@gmail.com>
2021-12-23 16:00:41 +0900

    ⚰️ :: Logging 삭제

--- src/main/java/com/example/zoom/global/config/SecurityConfig.java
+++ src/main/java/com/example/zoom/global/config/SecurityConfig.java
@@ -1,14 +1,12 @@
 package com.example.zoom.global.config;
 
 import com.example.zoom.global.security.jwt.JwtTokenProvider;
-import com.example.zoom.global.security.logger.RequestLogger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 
 @Configuration
@@ -17,8 +15,6 @@ import org.springframework.security.web.access.intercept.FilterSecurityIntercept
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
-    private final RequestLogger requestLogger;
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,8 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .anyRequest().authenticated()
-                .and().apply(new FilterConfigure(jwtTokenProvider))
-                .and().addFilterAfter(requestLogger, FilterSecurityInterceptor.class);
+                .and().apply(new FilterConfigure(jwtTokenProvider));
 
 
     }
