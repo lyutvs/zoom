fd1b89c3b04589afc701fd510dbcfa2cf9095b42
lyutvs <lyutvs@gmail.com>
2021-12-24 10:35:56 +0900

    ⚡️ :: 임포트 변경

--- src/main/java/com/example/zoom/domain/refresh_token/RefreshToken.java
+++ src/main/java/com/example/zoom/domain/refresh_token/RefreshToken.java
@@ -1,15 +1,14 @@
 package com.example.zoom.domain.refresh_token;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
 
-import javax.persistence.Id;
 
 @Getter
-@Builder
 @AllArgsConstructor
 @RedisHash
 public class RefreshToken {
@@ -17,6 +16,7 @@ public class RefreshToken {
     @Id
     private final String email;
 
+    @Indexed
     private String token;
 
     @TimeToLive
