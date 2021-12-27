6ddbe47cfa4eae8b3ac7b2bf669fd34b833d0dd2
lyutvs <lyutvs@gmail.com>
2021-12-27 23:11:51 +0900

    ⚡️ :: SecurityConfig

--- src/main/java/com/example/zoom/global/config/SecurityConfig.java
+++ src/main/java/com/example/zoom/global/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.example.zoom.global.config;
 
 import com.example.zoom.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -32,6 +35,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+	}
+
 
 
 }
