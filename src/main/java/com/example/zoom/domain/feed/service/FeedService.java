136a67a306206580793f450a5535c8949492f5a6
lyutvs <lyutvs@gmail.com>
2021-12-27 23:22:29 +0900

    ⚡️ :: feedService

--- src/main/java/com/example/zoom/domain/feed/service/FeedService.java
+++ src/main/java/com/example/zoom/domain/feed/service/FeedService.java
@@ -1,4 +1,9 @@
 package com.example.zoom.domain.feed.service;
 
+import com.example.zoom.domain.feed.presentation.dto.request.PostFeedRequest;
+
 public interface FeedService {
+
+    void postCarrotFeed(PostFeedRequest request);
+
 }
