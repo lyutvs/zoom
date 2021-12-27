9117d9efaab7a1098dc29d0aed24a1a64e4c08af
lyutvs <lyutvs@gmail.com>
2021-12-27 19:19:13 +0900

    📑 :: UserFacade

---
+++ src/main/java/com/example/zoom/domain/user/facade/UserFacade.java
@@ -0,0 +1,13 @@
+package com.example.zoom.domain.user.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserFacade {
+
+
+}
