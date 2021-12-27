994cfe7ddc8e4fa01a7a13944a10908d6c119b0b
lyutvs <lyutvs@gmail.com>
2021-12-27 23:11:02 +0900

    ⚡️ :: UserFacade

--- src/main/java/com/example/zoom/domain/user/facade/UserFacade.java
+++ src/main/java/com/example/zoom/domain/user/facade/UserFacade.java
@@ -1,5 +1,7 @@
 package com.example.zoom.domain.user.facade;
 
+import com.example.zoom.domain.user.domain.repository.UserRepository;
+import com.example.zoom.domain.user.exception.UserAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,5 +11,18 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class UserFacade {
 
+    private final UserRepository userRepository;
+
+    public boolean isAlreadyExists(String email) {
+        if(userRepository.findByEmail(email).isPresent()) {
+            throw UserAlreadyExistsException.EXCEPTION;
+        }
+        return true;
+    }
+
+    public void existsUseridFilter(String userid) {
+        if (userRepository.findByEmail(userid).isPresent())
+            throw UserAlreadyExistsException.EXCEPTION;
+    }
 
 }
