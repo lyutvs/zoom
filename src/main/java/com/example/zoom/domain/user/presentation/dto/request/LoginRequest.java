bedc0d6701dbbc290a84625a482e93412e754b36
lyutvs <lyutvs@gmail.com>
2021-12-27 19:19:33 +0900

    ⚡️ :: LoginRequest

--- src/main/java/com/example/zoom/domain/user/presentation/dto/request/LoginRequest.java
+++ src/main/java/com/example/zoom/domain/user/presentation/dto/request/LoginRequest.java
@@ -3,10 +3,17 @@ package com.example.zoom.domain.user.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor
 public class LoginRequest {
 
-    private String code;
+    @NotBlank(message = "email은 Null, 공백을 허용하지 않습니다.")
+    private String email;
+
+    @NotBlank(message = "password는 Null, 공백을 허용하지 않습니다.")
+    private String password;
 
 }
+
