36f7c397d1ab85abc297f7ca1102fb8a04dd777a
lyutvs <lyutvs@gmail.com>
2021-12-30 19:55:12 +0900

    ⚡️ :: FeedServiceImpl

--- src/main/java/com/example/zoom/domain/feed/service/FeedServiceImpl.java
+++ src/main/java/com/example/zoom/domain/feed/service/FeedServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.zoom.domain.feed.service;
 
 import com.example.zoom.domain.feed.domain.Feed;
+import com.example.zoom.domain.feed.domain.repository.FeedRepository;
 import com.example.zoom.domain.feed.facade.FeedFacade;
 import com.example.zoom.domain.feed.presentation.dto.request.PostFeedRequest;
+import com.example.zoom.domain.feed.presentation.dto.response.FeedResponse;
 import com.example.zoom.domain.user.domain.User;
 import com.example.zoom.domain.user.domain.repository.UserRepository;
 import com.example.zoom.domain.user.facade.UserFacade;
@@ -14,17 +16,23 @@ import org.springframework.stereotype.Service;
 public class FeedServiceImpl implements FeedService {
 
     private final UserRepository userRepository;
+    private final FeedRepository feedRepository;
     private final UserFacade userFacade;
     private final FeedFacade feedFacade;
 
     @Override
-    public void postCarrotFeed(PostFeedRequest request) {
+    public FeedResponse postFeed(PostFeedRequest request) {
         User user = userRepository.findById(userFacade.getUserEmail())
             .orElse(null);
 
-    Feed feed = feedFacade.getFeedById(feedId);
+        Feed feed = feedRepository.save(
+                Feed.builder()
+                        .title(request.getTitle())
+                        .description(request.getDescription())
+                        .build()
+        );
 
     	return feedFacade.feedToFeedResponse(feed, user);
-}
+    }
 
 }
