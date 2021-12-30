e81d2e595f7ebc43215c8a4ab8f5a20bce5831a2
lyutvs <lyutvs@gmail.com>
2021-12-30 19:55:52 +0900

    ⚡️ :: ImageRepository

--- src/main/java/com/example/zoom/domain/feed/domain/repository/ImageRepository.java
+++ src/main/java/com/example/zoom/domain/feed/domain/repository/ImageRepository.java
@@ -1,7 +1,12 @@
 package com.example.zoom.domain.feed.domain.repository;
 
+import com.example.zoom.domain.feed.domain.Feed;
 import com.example.zoom.domain.feed.domain.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.Optional;
+
 public interface ImageRepository extends JpaRepository<Image, Long> {
+    Optional<Image> findByFeedById(Feed feedId);
 }
