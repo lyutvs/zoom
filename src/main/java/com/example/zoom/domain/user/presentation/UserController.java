34cbaa78937ff381c86ae12241c5d58d0354ac02
lyutvs <lyutvs@gmail.com>
2022-01-18 00:10:18 +0900

    ⚡️ :: UserController

--- src/main/java/com/example/zoom/domain/user/presentation/UserController.java
+++ src/main/java/com/example/zoom/domain/user/presentation/UserController.java
@@ -1,9 +1,9 @@
 package com.example.zoom.domain.user.presentation;
 
-import com.example.zoom.domain.user.presentation.dto.request.LoginRequest;
+import com.example.zoom.domain.user.presentation.dto.request.SignInRequest;
 import com.example.zoom.domain.user.presentation.dto.request.SignUpRequest;
 import com.example.zoom.domain.user.service.UserService;
-import com.example.zoom.domain.refresh_token.presenstation.dto.TokenResponse;
+import com.example.zoom.domain.refresh_token.presenstation.dto.AccessTokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +16,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/auth")
-    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<AccessTokenResponse> login(@RequestBody SignInRequest request) {
 //        return userService.login(request);
         return null;
     }
