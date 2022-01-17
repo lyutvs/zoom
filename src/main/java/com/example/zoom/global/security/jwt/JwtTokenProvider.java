c7e693430b25f0137e882b5bc4cc9a09191f3790
lyutvs <lyutvs@gmail.com>
2022-01-17 23:49:00 +0900

    ♻️ :: JwtTokenProvider 변경

--- src/main/java/com/example/zoom/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/zoom/global/security/jwt/JwtTokenProvider.java
@@ -1,13 +1,12 @@
 package com.example.zoom.global.security.jwt;
 
-import com.example.zoom.domain.refresh_token.RefreshToken;
-import com.example.zoom.domain.refresh_token.domain.repository.RefreshTokenRepository;
-import com.example.zoom.domain.user.exception.ExpiredAccessTokenException;
-import com.example.zoom.domain.user.exception.ExpiredRefreshTokenException;
+import com.example.zoom.global.exception.ExpiredTokenException;
 import com.example.zoom.global.exception.InvalidTokenException;
 import com.example.zoom.global.security.jwt.auth.AuthDetailsService;
-import com.example.zoom.global.util.jwt.dto.TokenResponse;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -24,57 +23,28 @@ public class JwtTokenProvider {
 
     private final AuthDetailsService authDetailsService;
     private final JwtProperties jwtProperties;
-    private final RefreshTokenRepository refreshTokenRepository;
 
-    public TokenResponse generateToken(String username) {
-        String access = generateAccessToken(username);
-        String refresh = generateRefreshToken(username);
-
-        refreshTokenRepository.save(
-                RefreshToken.builder()
-                        .email(username)
-                        .refreshToken(refresh)
-                        .ttl(jwtProperties.getRefreshExp())
-                        .build()
-        );
-
-        return new TokenResponse(access, refresh);
+    public String generateAccessToken(String id) {
+        return generateToken(id, "access", jwtProperties.getAccessExp());
     }
 
-
-    public String generateAccessToken(String email) {
-        return Jwts.builder()
-                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
-                .setHeaderParam("typ", "JWT")
-                .setSubject(email)
-                .claim("type", "access")
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.getRefreshExp() * 1000))
-                .compact();
+    public String generateRefreshToken(String id) {
+        return generateToken(id, "refresh", jwtProperties.getRefreshExp());
     }
 
-    public String generateRefreshToken(String email) {
+    private String generateToken(String id, String type, Long exp) {
         return Jwts.builder()
                 .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
-                .setHeaderParam("typ", "JWT")
-                .setSubject(email)
-                .claim("type", "refresh")
+                .setSubject(id)
+                .claim("type", type)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.getRefreshExp() * 1000))
+                .setExpiration(new Date(System.currentTimeMillis() + exp))
                 .compact();
     }
 
     public String resolveToken(HttpServletRequest request) {
         String bearer = request.getHeader(jwtProperties.getHeader());
-        if(bearer != null && bearer.startsWith(jwtProperties.getPrefix())
-                && bearer.length() > jwtProperties.getPrefix().length() + 1)
-            return bearer.substring(jwtProperties.getPrefix().length() + 1);
-        return null;
-    }
-
-    public boolean validateToken(String token) {
-        return getTokenBody(token)
-                .getExpiration().after(new Date());
+        return parseToken(bearer);
     }
 
     public Authentication authentication(String token) {
@@ -83,33 +53,22 @@ public class JwtTokenProvider {
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
-    public String parseRefreshToken(String token) {
-        try{
-
-            Claims claims = Jwts.parser().setSigningKey(jwtProperties.getSecretKey())
-                    .parseClaimsJws(token).getBody();
-            if (claims.get("type").equals("refresh")) {
-                return claims.getSubject();
-            }
-            throw new InvalidTokenException();
-
-        } catch (ExpiredJwtException e) {
-            throw new ExpiredRefreshTokenException();
-        }
+    public String parseToken(String bearerToken) {
+        if (bearerToken != null && bearerToken.startsWith(jwtProperties.getPrefix()))
+            return bearerToken.replace(jwtProperties.getPrefix(), "");
+        return null;
     }
 
     private Claims getTokenBody(String token) {
-        if(token == null)
-            throw new InvalidTokenException();
+
         try {
             return Jwts.parser().setSigningKey(jwtProperties.getSecretKey())
                     .parseClaimsJws(token).getBody();
-        } catch (ExpiredJwtException e) {
-            throw new ExpiredAccessTokenException();
-        } catch (MalformedJwtException | SignatureException e) {
-            throw new InvalidTokenException();
+        } catch (ExpiredTokenException e) {
+            throw ExpiredTokenException.EXCEPTION;
+        } catch (Exception e) {
+            throw InvalidTokenException.EXCEPTION;
         }
-
     }
 
     private String getTokenSubject(String token) {
